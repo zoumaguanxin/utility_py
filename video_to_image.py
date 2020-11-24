4cd2e9b45dfa93ac68d0ea424fdb64a5260c3771
shaoan <zsa5370@foxmail.com>
2020-11-24 14:41:31 +0800

    improve vedio to image

--- video_to_image.py
+++ video_to_image.py
@@ -1,26 +1,52 @@
 import cv2
 import numpy as np
- 
+import os
+import argparse
+
+
 
 def save_image(image,addr,num):
-    address = addr + str(num)+ '.jpg'
-    cv2.imwrite(address,image)
- 
+	n = "%04d" % num
+	# str_n = str(num)
+	# str_n = str_n.zfill(4)
+	address = os.path.join(addr, str(n)+ '.jpg')
+	cv2.imwrite(address,image)
 
-videoCapture = cv2.VideoCapture("/home/shawn/Videos/yolo_cup/video/VID_20200807_112757.mp4")
+parser = argparse.ArgumentParser(description='Process some integers.')
+args = parser.add_argument('--dir',dest='dir',type=str,help='vedio directory')
+args = parser.parse_args()
+
+vedio_dir = args.dir
+
+fname,fename=os.path.split(vedio_dir)
+
+videoCapture = cv2.VideoCapture(vedio_dir)
 
 
 # videoCapture=cv2.VideoCapture(1)
- 
+
+image_dir = os.path.join(fname,"image/")
+if os.path.exists(image_dir):
+	for i in range(99999):
+		tem = "image"
+		tem = tem+str(i)
+		image_dir = os.path.join(fname,tem)
+		if os.path.exists(image_dir):
+			continue
+		else:
+			os.mkdir(image_dir)
+			break
+else:
+	os.mkdir(image_dir)
 
 success, frame = videoCapture.read()
 i = 0
-timeF = 12
+timeF = 4
 j=0
 while success :
-    i = i + 1
-    if (i % timeF == 0):
-        j = j + 1
-        save_image(frame,'/home/shawn/Videos/yolo_cup/video/images_VID_20200807_112757/',j)
-        print('save image:',i)
-    success, frame = videoCapture.read()
+	i = i + 1
+	if (i % timeF == 0):
+		j = j + 1
+		save_image(frame,image_dir,j)
+		print('save image:',i)
+	success, frame = videoCapture.read()
